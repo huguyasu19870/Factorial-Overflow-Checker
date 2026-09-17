9224dd3b68944f7d7a2991595959e64c90d224eb
huguyasu19870 <137283325+huguyasu19870@users.noreply.github.com>
2026-09-18 08:51:46 +0900

    Added support for other signed integer type.

--- main.cpp
+++ main.cpp
@@ -1,23 +1,95 @@
 #include <iostream>
+#include <array>
 
 int main(){
-    int n;
-    bool hasOverflowed = false;
+    short int sintNum;
+    int intNum;
+    long int lintNum;
+    long long int llintNum;
+    unsigned short int usintNum;
+    unsigned int uintNum;
+    unsigned long int ulintNum;
+    unsigned long long int ullintNum;
+    bool sintOvf = false;
+    bool intOvf = false;
+    bool lintOvf = false;
+    bool llintOvf = false;
+    bool usintOvf = false;
+    bool uintOvf = false;
+    bool ulintOvf = false;
+    bool ullintOvf = false;
+    bool allOvf = false;
+    int overflowedNums[8];
     for(int i = 1; i < 100; i++){
         for(int o = i; o > 0; o--){
             if(o == i){
-                n = o;
+                sintNum = o;
+                intNum = o;
+                lintNum = o;
+                llintNum = o;
+                usintNum = o;
+                uintNum = o;
+                ulintNum = o;
+                ullintNum = o;
                 continue;
             }
-            n *= o;
-            if((n) < 0){
-                hasOverflowed = true;
+            if(((sintNum) > (sintNum*o)) && !sintOvf){
+                overflowedNums[0] = i;
+                sintOvf = true;
+            }
+            if(((intNum) > (intNum*o)) && !intOvf){
+                overflowedNums[1] = i;
+                intOvf = true;
+            }
+            if(((lintNum) > (lintNum*o)) && !lintOvf){
+                overflowedNums[2] = i;
+                lintOvf = true;
+            }
+            if(((llintNum) > (llintNum*o)) && !llintOvf){
+                overflowedNums[3] = i;
+                llintOvf = true;
+            }
+            if(((usintNum) > (usintNum*o)) && !usintOvf){
+                overflowedNums[4] = i;
+                usintOvf = true;
+            }
+            if(((uintNum) > (uintNum*o)) && !uintOvf){
+                overflowedNums[5] = i;
+                uintOvf = true;
+            }
+            if(((ulintNum) > (ulintNum*o)) && !ulintOvf){
+                overflowedNums[6] = i;
+                ulintOvf =true;
+            }
+            if(((ullintNum) > (ullintNum*o)) && !ullintOvf){
+                overflowedNums[7] = i;
+                ullintOvf = true;
+            }
+            //allOvf = (sintOvf && intOvf && lintOvf && llintOvf && usintOvf && uintOvf && ulintOvf && ullintOvf);
+            allOvf = (sintOvf && intOvf && lintOvf && llintOvf);
+            sintNum *= o;
+            intNum *= o;
+            lintNum *= o;
+            llintNum *= o;
+            /*
+            usintNum *= o;
+            uintNum *= o;
+            ulintNum *= o;
+            ullintNum *= o;*/
+            if(allOvf){
                 break;
             }
         }
-        std::cout << "Factorial of " << i << " is: " << n << std::endl;
-        if(hasOverflowed){
-            std::cout << "Factorial overflowed at " << i << std::endl;
+        std::cout << "Factorial of " << i << " is: " << llintNum << std::endl;
+        if(allOvf){
+            std::cout << "Factorial with type short int overflowed at " << overflowedNums[0] << std::endl;
+            //std::cout << "Factorial with type unsigned short int overflowed at " << overflowedNums[4] << std::endl;
+            std::cout << "Factorial with type int overflowed at " << overflowedNums[1] << std::endl;
+            //std::cout << "Factorial with type unsigned int overflowed at " << overflowedNums[5] << std::endl;
+            std::cout << "Factorial with type long int overflowed at " << overflowedNums[2] << std::endl;
+            //std::cout << "Factorial with type unsigned long int overflowed at " << overflowedNums[6] << std::endl;
+            std::cout << "Factorial with type long long int overflowed at " << overflowedNums[3] << std::endl;
+            //std::cout << "Factorial with type unsigned long long int overflowed at " << overflowedNums[7] << std::endl;
             break;
         }
     }
